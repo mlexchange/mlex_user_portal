9ba73f2290a7a9b779fa6256cf554978fd605bc7
eaholman <eaholman@lbl.gov>
2022-04-13 12:06:56 -0700

    Created user_portal registration page front-end.

--- user_portal/src/user_portal.py
+++ user_portal/src/user_portal.py
@@ -4,8 +4,10 @@
 # import dash_core_components as dcc
 # import dash_table
 
-from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
+from dash import Dash, callback, callback_context, html, dcc, dash_table, Input, Output, State, MATCH, ALL
 import dash_bootstrap_components as dbc
+
+import requests
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, "../assets/segmentation-style.css",]  # need to use bootstrap themes
 
@@ -29,7 +31,7 @@ header = dbc.Navbar(
                         [
                             html.Div(
                                 [
-                                    html.H3("MLExchange | User Portal"),
+                                    html.H3("MLExchange | User Registration"),
                                 ],
                                 id="app-title",
                             )
@@ -48,18 +50,94 @@ header = dbc.Navbar(
     sticky="top",
 )
 
+input_groups = html.Div(
+    [
+        dbc.InputGroup(
+            [dbc.InputGroupText("Registrant's First Name: "), dbc.Input(id="fname", placeholder="First Name of User")],
+            className="mb-3",
+        ),
+        dbc.InputGroup(
+            [dbc.InputGroupText("Registrant's Last Name: "), dbc.Input(id="lname", placeholder="Last Name of User")],
+            className="mb-3",
+        ),
+        dbc.InputGroup(
+            [
+                dbc.InputGroupText("Email Address: "),
+                dbc.Input(id="email_01", placeholder="Email Username"),
+                dbc.InputGroupText("@"),
+                dbc.Input(id="email_02", placeholder="Email Domain")
+            ],
+            className="mb-3",
+        ),
+        dbc.InputGroup(
+            [dbc.InputGroupText("Registrant's ORCID: "), dbc.Input(id="orcid", placeholder="ORCID of User")],
+            className="mb-3",
+        )
+    ]
+)
+
+# Metadata
+meta = html.Div(
+    children = [dcc.Store(id="nothing", data='')],
+    id="no-display"
+    ),
+
 # Setting up initial webpage layout
 app.layout = html.Div (
         [
             header,
             dbc.Container(
-                [
-                    dbc.Row([dbc.Col(width=6), dbc.Col(width=6)]),
-                    dbc.Row(dbc.Col(width=12)),
-                    #dbc.Row(meta)
-                ]
+                dbc.Row(
+                    dbc.Card(
+                        children=[
+                            dbc.CardHeader("Welcome to the MLExchange Platform. In order to access our platform " +
+                                "and use the MLExchange community resources, please register using the form below." +
+                                " Please note that inaccuracy in provided information may delay the registration process."),
+                            dbc.CardBody(
+                                dbc.Row(
+                                    dbc.Col(
+                                        html.Div([
+                                            input_groups,
+                                            dbc.Button(
+                                                "Register",
+                                                id="register-user",
+                                                className="mr-1",
+                                                color="success",
+                                                size="lg",
+                                                style={'width':'100%', 'margin': '10px', 'margin-left': '0px'})]),
+                                        width = 8),
+                                        align="center", justify="center")
+                            )
+                        ]
+                    )
+                )
             ),
-])
+            dbc.Row(meta)
+        ]
+    )
+
+## REACTIVE CALLBACKS ##
+@app.callback(
+    Output("nothing", "data"),
+    Input("register-user", "n_clicks"),
+    State("fname","value"),
+    State("lname","value"),
+    State("email_01","value"),
+    State("email_02","value"),
+    State("orcid","value"),
+    prevent_initial_call=True
+    )
+def create_user(n, fname, lname, email_01, email_02, orcid):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    email = str(email_01) + "@" + str(email_02)
+    user_data = {'fname':fname, 'lname':lname, 'email':email, 'orcid':orcid}
+
+    url = "http://user-api:5000/api/v0/users/"
+    
+    if "register-user" in changed_id:
+        print(fname)
+        requests.post(url, json=user_data)
+    return ""
 
 # for testing interface
 if __name__ == "__main__":
