fb9f119035e7afcb0005f0cddc028f1d474df194
Elizabeth Holman <eaholman@lbl.gov>
2022-04-14 14:59:49 -0700

    Update mlex_login.py
    Updated annotation to clarify placeholder code.

--- user_portal/src/mlex_login.py
+++ user_portal/src/mlex_login.py
@@ -1,3 +1,5 @@
+#placeholder file for Hari's login page
+
 from dash import Dash, callback, callback_context, html, dcc, dash_table, Input, Output, State, MATCH, ALL
 import dash_bootstrap_components as dbc
 
